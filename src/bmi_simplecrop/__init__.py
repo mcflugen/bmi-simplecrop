e23bd9951d3a2229f46875f0bc56b5449746e5aa
mcflugen <mcflugen@gmail.com>
2022-07-04 11:28:47 -0600

    Revert "add bmi_simplecrop package"
    This reverts commit 1cb66b53bba102f949a1ddc733217c80c4f8b797.

--- src/bmi_simplecrop/__init__.py
+++
@@ -1,7 +0,0 @@
-from .bmi_simplecrop import BmiSimpleCrop
-from .bmi_simplecrops import BmiSimpleCrops
-from .simplecrop import SimpleCrop, SimpleCrops
-from ._version import __version__
-
-
-__all__ = ["__version__", "BmiSimpleCrop", "BmiSimpleCrops", "SimpleCrop", "SimpleCrops"]
